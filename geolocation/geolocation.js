39d06b04f810cb3e2061a69dab5020fd1b64af99
Nicolas Renvoisé <n.renvoise@ubitransport.com>
2020-07-01 17:52:37 +0200

    feat: add geolocation

---
+++ geolocation/geolocation.js
@@ -0,0 +1,84 @@
+function Geolocator()
+{
+    let currentGeolocationId = null;
+
+    /**
+     * Check if the navigator handles geolocation
+     *
+     * @returns {boolean}
+     */
+    this.isGeolocationAvailable = function () {
+
+        if ("geolocation" in navigator) {
+            return true;
+        }
+
+        return false;
+    };
+
+    /**
+     * Geolocate the user
+     *
+     * @param successCallback
+     * @param failureCallback
+     * @param duration to stop the watch after a while
+     * @param options
+     * @returns {number}
+     */
+    this.watchPosition = function (
+        successCallback,
+        failureCallback = null,
+        duration = null,
+        options = {}
+    ) {
+        if (currentGeolocationId) {
+            return;
+        }
+
+        if (!this.isGeolocationAvailable()) {
+            throw new Error("Your navigator does not handle geolocation.");
+        }
+
+        if (!options.enableHighAccuracy) {
+            options.enableHighAccuracy = true;
+        }
+
+        // Duration in ms before the failure callback is called
+        if (!options.timeout) {
+            options.timeout = 10000;
+        }
+
+        if (!options.maximumAge) {
+            options.maximumAge = 0;
+        }
+
+        currentGeolocationId = navigator.geolocation.watchPosition(successCallback, failureCallback, options);
+
+        if (duration && duration !== Infinity) {
+            setTimeout(this.clearWatch, duration);
+        }
+    },
+
+    /**
+     * Stop the geolocation
+     */
+    this.clearWatch = function () {
+        navigator.geolocation.clearWatch(currentGeolocationId);
+    }
+}
+
+const geolocator = new Geolocator();
+
+const geolocationIsAvailable = geolocator.isGeolocationAvailable();
+
+if (!geolocationIsAvailable) {
+    console.warn('Geolocation is not available.');
+} else {
+    console.log('Geolocation available.');
+}
+
+geolocator.watchPosition(
+    (position) => console.log('watching...', position),
+    (error) => console.log('geolocation error!', error),
+    3000
+);
