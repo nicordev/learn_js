5a1cdf1d0fe2f4cfba4fe35be4bcb2432141eba9
Nicolas Renvoisé <n.renvoise@ubitransport.com>
2021-02-02 22:26:42 +0100

    refactor promises

--- promises/promises.js
+++ promises/promises.js
@@ -1,9 +1,54 @@
-const myExecutor = function (resolve, reject) {
-    console.log('Initialisation...');
-    setTimeout(resolve, 2000);
-    setTimeout(reject, 3000);
-}
+const createMessagesWrapperElement = () => {
+    const wrapperElement = document.createElement('div');
+    wrapperElement.classList.add('message-wrapper');
+    console.log('creating wrapper element...');
+    return wrapperElement;
+};
 
-const myPromise = new Promise(myExecutor);
-myPromise.then(() => console.log('Success!'));
-myPromise.catch(() => console.log('Rejected...'));
+const createMessageElement = (message) => {
+    const messageElement = document.createElement('div');
+    messageElement.classList.add('message');
+    messageElement.textContent = message;
+    console.log('creating message element...');
+    return messageElement;
+};
+
+const addElementToBody = (element) => {
+    console.log('appending element...');
+    document.body.appendChild(element);
+    return element;
+};
+
+const initialFunctionExecutedOnSuccess = () => {
+    console.log("Let's go!");
+    return createMessagesWrapperElement();
+};
+const initialFunctionExecutedOnError = () => {
+    throw Error('Error at the beginning...');
+    console.log('this statement is never reached');
+};
+
+const handleFailure = (error) => console.log('From the catch function:', error);
+
+const tryWithAnError = false;
+
+const build = new Promise((myResolveFunction, myRejectFunction) => {
+    if (tryWithAnError) {
+        myRejectFunction();
+    }
+    myResolveFunction();
+});
+
+addElementToBody(createMessageElement('Try Promise...'));
+
+build
+    .then(initialFunctionExecutedOnSuccess, initialFunctionExecutedOnError)
+    .then((wrapperElement) => {
+        wrapperElement.appendChild(createMessageElement('Hello World!'));
+        return wrapperElement;
+    })
+    .then(addElementToBody)
+    .then((element) => console.log('Success! Message added!', element))
+    .catch(handleFailure);
+
+addElementToBody(createMessageElement('Is it working?'));
