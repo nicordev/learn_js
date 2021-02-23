9082ce8b66b1c7399cbf2d791d5afd70f9057a69
Nicolas Renvoisé <n.renvoise@ubitransport.com>
2021-02-23 22:19:37 +0100

    add variable type

---
+++ variable_type/variable_type.js
@@ -0,0 +1,14 @@
+const basket = {
+    owner: 'sarah',
+    fruits: [
+        'apple',
+        'pear',
+        'cherry'
+    ]
+}
+
+console.log(typeof basket);
+console.log(typeof basket.fruits);
+console.log(Array.isArray(basket.fruits));
+console.log(typeof 1);
+console.log(typeof '1');
