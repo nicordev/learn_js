6aba3d860b218e15d510d47de9c6ce268e1961c7
nicolas renvoise <nicolas.renvoise@alximy.io>
2023-05-14 13:07:28 +0200

    improve music countdown

--- audio/music/audio.js
+++ audio/music/audio.js
@@ -1,41 +1,90 @@
-function MusicPlayer(delayBeforeNextLoop) {
-    const audio = new Audio("media/canon-pachelbel-432hz.wav");
-    let endedAt = undefined;
-    let intervalId = undefined;
+function Countdown(durationInSeconds) {
+    let duration = durationInSeconds;
+    let counterId = undefined;
+    let callBackOnSecond = console.log;
 
-    const showRemainingTimeBeforeNextLoop = function () {
-        if (typeof endedAt === 'undefined') {
-            return;
+    const countdown = () => {
+        callBackOnSecond(duration);
+        --duration;
+
+        if (duration <= 0) {
+            duration = 0;
         }
+    };
 
-        const elapsedTime = Date.now() - endedAt;
-        const remainingTimeBeforeNextLoop = delayBeforeNextLoop - elapsedTime;
+    this.setDuration = function (durationInSeconds) {
+        duration = durationInSeconds;
+    };
 
+    this.setCallbackOnSecond = function (callback) {
+        callBackOnSecond = callback;
+    };
+
+    this.start = function (durationInSeconds) {
+        duration = durationInSeconds;
+        // countdown
+        counterId = setInterval(() => {
+            countdown(duration);
+        }, 1000);
+
+        // stop counting
+        setTimeout(() => {
+            this.stop();
+        }, duration * 1000 + 900);
+
+        // first time
+        countdown(duration);
+    };
+
+    this.stop = function () {
+        clearInterval(counterId);
+    };
+
+    this.reset = function () {
+        duration = durationInSeconds;
+    };
+}
+
+function MusicPlayer(delaysBetweenLoops) {
+    console.log(delaysBetweenLoops);
+
+    const audio = new Audio("media/canon-pachelbel-432hz.wav");
+    let loopId = 0;
+    const incrementLoopId = () => {
+        ++loopId;
+
+        if (loopId >= delaysBetweenLoops.length) {
+            loopId = 0;
+        }
+    };
+    const countdown = new Countdown(delaysBetweenLoops[loopId]);
+
+    const showRemainingTimeBeforeNextLoop = function (
+        remainingTimeBeforeNextLoop
+    ) {
         console.log(`next loop in: ${remainingTimeBeforeNextLoop} s`);
-        document.querySelector('.next-loop').textContent = `next loop in: ${remainingTimeBeforeNextLoop} s`
-    }
+        document.querySelector(
+            ".next-loop"
+        ).textContent = `next loop in: ${remainingTimeBeforeNextLoop} s`;
+    };
 
-    audio.addEventListener('ended', function (event) {
+    audio.addEventListener("ended", function (event) {
         endedAt = Date.now();
-        showRemainingTimeBeforeNextLoop();
 
-        // count remaining time before next loop
-        intervalId = setInterval(
-            function () {
-                showRemainingTimeBeforeNextLoop()
-            },
-            1000,
+        // select next delay
+        incrementLoopId();
+        console.log({ loopId, nextDelay: delaysBetweenLoops[loopId] });
+
+        // countdown
+        countdown.start(
+            delaysBetweenLoops[loopId],
+            showRemainingTimeBeforeNextLoop
         );
 
         // set next loop
-        setTimeout(
-            function () {
-                audio.play();
-                delayBeforeNextLoop = Math.round(5 * 60 * Math.random());
-                clearInterval(intervalId);
-            },
-            delayBeforeNextLoop * 1000,
-        );
+        setTimeout(function () {
+            audio.play();
+        }, delaysBetweenLoops[loopId] * 1000 + 900);
     });
 
     this.play = function () {
@@ -49,16 +98,18 @@ function MusicPlayer(delayBeforeNextLoop) {
     return this;
 }
 
-const musicPlayer = new MusicPlayer(60 * 5 + 38);
+const musicPlayer = new MusicPlayer([60 * 5, 60 * 2, 60 * 8]);
 
-document.querySelector('.play-button').addEventListener('click', function (event) {
-    if (this.textContent === 'Pause') {
-        musicPlayer.pause();
-        this.textContent = 'Play';
+document
+    .querySelector(".play-button")
+    .addEventListener("click", function (event) {
+        if (this.textContent === "Pause") {
+            musicPlayer.pause();
+            this.textContent = "Play";
 
-        return;
-    }
+            return;
+        }
 
-    this.textContent = 'Pause';
-    musicPlayer.play();
-});
+        this.textContent = "Pause";
+        musicPlayer.play();
+    });
